7bbddf9273a1f44339d50e86b6cbfe4e2a057876
samuel <samuelmmanor@gmail.com>
2022-01-12 17:44:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(driver => driver === string || driver === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(driver => driver.charAt(0) === string)
+}
+
+function matchName(array, string) {
+    return array.filter(driverName => driverName.name === string)
+}
